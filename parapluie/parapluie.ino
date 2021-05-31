7b9979ff7d87d33ee5c4b08c9f33c76daa84546a
Thomas Vernet <thomas.vernet.1@gmail.com>
2021-05-31 11:37:44 +0200

    FEAT/ARDUINO: ADD WIFIMANAGER SUPPORT
    Added basic exemple for tzapu/WiFiManager with AP ssid="test" and pw="testtest"

--- parapluie/parapluie.ino
+++ parapluie/parapluie.ino
@@ -48,6 +48,8 @@
 #include <Servo.h>
 #include <ESP8266WebServer.h>
 
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+
 ESP8266WebServer  server(80);                         //declaration du serveur web sur le port 80
 
 const char* nomDuReseau = "xxxxxxxxx";               // Nom du réseau wifi local (Ce qu'on appelle le SSID).
@@ -288,16 +290,45 @@ monservo.detach();     // débrancher le servomoteur de la broche D3 (GPIO 0)
 }
 
 void connexion() {
-  WiFi.mode(WIFI_STA); // Le D1 mini est en mode station wifi.
-  Serial.println("Connexion");
-  WiFi.begin(nomDuReseau, motDePasse);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("Connecté");
-  Serial.print("Station IP address: ");
-  Serial.println(WiFi.localIP());
-  pinMode(LED_BUILTIN, OUTPUT);   // Configure la broche D4 (GPIO 2) sur la quelle est branchée la LED_BUILTIN en sortie
-  digitalWrite(LED_BUILTIN, LOW); // Allume la LED_BUILTIN
+//  WiFi.mode(WIFI_STA); // Le D1 mini est en mode station wifi.
+//  Serial.println("Connexion");
+//  WiFi.begin(nomDuReseau, motDePasse);
+//  while (WiFi.status() != WL_CONNECTED) {
+//    delay(500);
+//    Serial.print(".");
+//  }
+//  Serial.println("Connecté");
+//  Serial.print("Station IP address: ");
+//  Serial.println(WiFi.localIP());
+//  pinMode(LED_BUILTIN, OUTPUT);   // Configure la broche D4 (GPIO 2) sur la quelle est branchée la LED_BUILTIN en sortie
+//  digitalWrite(LED_BUILTIN, LOW); // Allume la LED_BUILTIN
+
+WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+
+    // WiFi.mode(WiFi_STA); // it is a good practice to make sure your code sets wifi mode how you want it.
+
+    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wm;
+
+    //reset settings - wipe credentials for testing
+    //wm.resetSettings();
+
+    // Automatically connect using saved credentials,
+    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+    // then goes into a blocking loop awaiting configuration and will return success result
+
+    bool res;
+    // res = wm.autoConnect(); // auto generated AP name from chipid
+    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+    res = wm.autoConnect("test","testtest"); // password protected ap
+
+    if(!res) {
+        Serial.println("Failed to connect");
+        // ESP.restart();
+    }
+    else {
+        //if you get here you have connected to the WiFi
+        Serial.println("connected...yeey :)");
+    }
 }
